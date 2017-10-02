c8711f69ddf5e94c0f0413f7f4ebee803f49d452
Weidong Yang <weidong@kineviz.com>
2017-10-01 17:26:55 -0700

    add line drawing

---
+++ nodejs/public/appDraw.js
@@ -0,0 +1,65 @@
+var socket = io.connect(window.location.origin);
+
+var heart_d = 0
+socket.on('heart', d=>{
+	console.log(d)
+	heart_d = d.val
+})
+
+const width = 800, height = 800
+
+function setup() {
+	createCanvas(width, height);
+	background(200)
+	
+}
+
+let mouseX0 = null
+let mouseY0 = null
+let bDrawing = false
+
+function draw() {
+	let red = 255-(heart_d)/4
+	// background(red, 50, 50)
+	fill(204, 101, 192, 5);
+  stroke(127, 63, 120);
+
+  ellipse(width/2, height/2, 600, 600)
+  // A rectangle
+  // rect(40, 120, 120, 40);
+  // rect(10*(heart_d-60), 120, 120, 40);
+  // An ellipse
+	// ellipse(240, 240, 80, 80);
+	
+	let r = (heart_d-10)/2
+	noStroke()
+	ellipse(width/2, height/2, r, r);
+
+  // A triangle
+  // triangle(300, 100, 320, 100, 310, 80);
+
+  // // A design for a simple flower
+  // translate(580, 200);
+  // noStroke();
+  // for (var i = 0; i < 10; i ++) {
+  //   ellipse(0, 30, 20, 80);
+  //   rotate(PI/5);
+  // }
+
+}
+
+
+function mousePressed(){
+  bDrawing = !bDrawing
+}
+
+function mouseMoved(){
+  if(bDrawing){
+    fill(0,0,0,255)
+    stroke(0)
+    line(mouseX0, mouseY0, mouseX, mouseY)
+  }
+  // ellipse(mouseX, mouseY, 28, 28)
+  mouseX0 = mouseX
+  mouseY0 = mouseY
+}
